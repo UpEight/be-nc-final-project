d311a938e9b75810bc6e890a59fbcf07f3f2c940
UpEight <milnerslm@gmail.com>
2019-11-15 11:11:22 +0000

    Add proof of concept POST and PATCH functions

--- service/handlers/users_handler.py
+++ service/handlers/users_handler.py
@@ -2,22 +2,19 @@ import json
 
 import tornado.web
 
+from pymongo import ReturnDocument
+
 
 class UsersHandler(tornado.web.RequestHandler):
     async def get(self, name):
         user = await self.settings["mongo_db"].users_collection.find_one(
-            {"name": name})
+            {"name": name}, {"_id": 0})
 
         if user is None:
             raise tornado.web.HTTPError(
                 404, f"Missing user: {name}")
 
-        print(user['_id'])
-
-        # self.finish(json.dumps(user.__class__))
-
-        self.finish(json.dumps(
-            {'name': user['name'], 'interests': user['interests']}))
+        self.finish(json.dumps(user))
 
     async def post(self, name):
         await self.settings["mongo_db"].users_collection.replace_one(
@@ -25,10 +22,19 @@ class UsersHandler(tornado.web.RequestHandler):
             {
                 "name": name,
                 "interests": json.loads(self.request.body)['interests'],
-                "fruit": json.loads(self.request.body)['fruit'],
-                "object": {"object1": json.loads(self.request.body)["object1"]}
+                "fruit": json.loads(self.request.body)['fruit']
             },
             upsert=True)
 
         self.set_status(204)
         self.finish()
+
+    async def patch(self, name):
+        user = await self.settings["mongo_db"].users_collection.find_one_and_update(
+            {"name": name},
+            {"$push": {"fruit": json.loads(
+                self.request.body)}}, projection={"_id": 0}, return_document=ReturnDocument.AFTER
+        )
+
+        self.set_status(200)
+        self.finish(json.dumps(user))
