745037c46ab37b7e01eb43c6a6eb6ed3eddc65cb
UpEight <milnerslm@gmail.com>
2019-11-18 16:51:53 +0000

    Add error handling to POST method

--- service/handlers/add_user_handler.py
+++ service/handlers/add_user_handler.py
@@ -7,8 +7,14 @@ from pymongo import ReturnDocument
 
 class AddUserHandler(tornado.web.RequestHandler):
     async def post(self):
+        if (json.loads(self.request.body)["uuid"] == ""):
+            raise tornado.web.HTTPError(
+                400, f"Bad Request")
+
         new_user = await self.settings["mongo_db"].users_collection.insert_one(
             json.loads(self.request.body))
+        print(new_user.inserted_id)
 
-        self.set_status(204)
-        self.finish("new user added")
+        if (new_user.acknowledged == True):
+            self.set_status(200)
+            self.finish(json.dumps(json.loads(self.request.body)))
