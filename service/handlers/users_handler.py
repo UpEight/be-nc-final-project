4c50599cac4fc18a1d88bc2e134f1d0c75263351
UpEight <milnerslm@gmail.com>
2019-11-20 11:27:03 +0000

    Move agenda item from 'going' to 'history' if date is passed when user object fetched

--- service/handlers/users_handler.py
+++ service/handlers/users_handler.py
@@ -4,10 +4,11 @@ import tornado.web
 
 from pymongo import ReturnDocument
 
+from datetime import datetime
+
 
 class UsersHandler(tornado.web.RequestHandler):
     async def get(self, uuid):
-        print(uuid)
         user = await self.settings["mongo_db"].users_collection.find_one(
             {"uuid": uuid}, {"_id": 0})
 
@@ -15,6 +16,27 @@ class UsersHandler(tornado.web.RequestHandler):
             raise tornado.web.HTTPError(
                 404, f"Missing user: {uuid}")
 
+        current_date = datetime.today()
+        start_of_current_date = current_date.replace(hour=00, minute=00)
+
+        current_agenda = user["Agenda"]["going"]
+        history = user["Agenda"]["history"]
+
+        for item in current_agenda:
+            meeting_date = item["date"]
+            datetime_object = datetime.strptime(meeting_date, '%Y-%m-%d')
+            end_of_meeting_date = datetime_object.replace(hour=23, minute=59)
+
+            if (start_of_current_date > end_of_meeting_date):
+                history.append(item)
+                current_agenda.remove(item)
+
+        user = await self.settings["mongo_db"].users_collection.find_one_and_update(
+            {"uuid": uuid},
+            {"$set": {"Agenda.going": current_agenda, "Agenda.history": history}}, projection={"_id": 0}, return_document=ReturnDocument.AFTER
+        )
+
+        self.set_status(200)
         self.finish(json.dumps(user))
 
     async def patch(self, uuid):
